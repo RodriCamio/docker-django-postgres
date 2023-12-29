53390b89576d966420ebd5ad4d980e3e832f8167
Rodrigo Camio <rodricamio94@gmail.com>
2023-12-29 01:39:37 -0300

    Modificaciones de seguridad, en cuanto a credenciales para la base de datos

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 # UPDATE
-sudo apt-get update 
-
+sudo apt-get update
 # DOCKER INSTALL
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 # DOCKER-COMPOSE INSTALL
 sudo apt-get update
 sudo apt-get install docker-compose-plugin
-
 # USE DOCKER WITHOUT SUDO
 sudo groupadd docker
 sudo usermod -aG docker vagrant
 newgrp docker
-
 # VERSION
 docker --version
 docker compose version
